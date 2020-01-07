ff64fb9cb311098ddc1f158f6679e58a9cdca31d
Michael Le <mnle529@gmail.com>
2020-01-06 23:39:33 -0800

    updated CSS, still need to work on search history

--- script.js
+++ script.js
@@ -7,15 +7,11 @@ var weatherKey = "&units=imperial&APPID=3f57d1d0ccb27e3179f39171730967ec";
 
 var searchBar = $("#search-bar");
 var searchBtn = $("#search-btn");
-var searchHis = $("#searchHistor");
+var history = $("#history");
 var currentCast = $(".currentCast");
-var storedHistory = [];
+var searchSaves = [];
 var forecastURL;
 var currentWeatherURL;
-
-var tempSearchHis = localStorage.getItem("storedHistory");
-if (tempSearchHis != null)
-    storedHistory = tempSearchHis.split(",");
 
 // Search button click event
 searchBtn.on("click", function () {
@@ -40,6 +36,7 @@ function generateCurrentWeather() {
     }).then(function (response) {
         console.log(response);
 
+        //Stores the weather data 
         currentWeatherInfo = {
             location: response.name,
             date: currentDate,
@@ -47,9 +44,11 @@ function generateCurrentWeather() {
             temp: Math.round(response.main.temp),
             wind: response.wind.speed,
             humidity: response.main.humidity,
-            uvIndex: 0
+            uvIndex: 0,
+            uvLevel: ""
         };
 
+        // UV call
         var lat = response.coord.lat;
         var long = response.coord.lon;
         var uvURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + long + "&appid=" + APIKey;
@@ -60,21 +59,33 @@ function generateCurrentWeather() {
         }).then(function (responseUV) {
             console.log(responseUV);
             currentWeatherInfo.uvIndex = responseUV.value;
+
+            // This is the assign the UV levels
+            if (currentWeatherInfo.uvIndex >= 8)
+                currentWeatherInfo.uvLevel = "high";
+            else if (currentWeatherInfo.uvIndex < 3)
+                currentWeatherInfo.uvLevel = "low";
+            else
+                currentWeatherInfo.uvLevel = "medium";
+
+
+            console.log(currentWeatherInfo);
+
+            // This generates a card with all the weather data for the searched location (generates it .currentCast)
+            var currentForecast = $('<div class="card"><div class="card-body"><h5 class="card-title">' + currentWeatherInfo.location + ' (' + currentWeatherInfo.date + ') ' +
+                '<img id="weather-icon" src="http://openweathermap.org/img/wn/' + currentWeatherInfo.weatherIcon + '@2x.png"></h5>' +
+                '<p class="card-text">Temperature: ' + currentWeatherInfo.temp + ' °F</p>' +
+                '<p class="card-text">Humidity: ' + currentWeatherInfo.humidity + '%</p>' +
+                '<p class="card-text">Wind Speed: ' + currentWeatherInfo.wind + ' MPH</p>' +
+                '<p class="card-text">UV Index: <span class="badge badge-secondary ' + currentWeatherInfo.uvIndex + '">' + currentWeatherInfo.uvLevel + '</span>')
+            $(".currentCast").append(currentForecast);
+
         });
-
-        console.log(currentWeatherInfo);
-
-        var currentForecast = $('<div class="card"><div class="card-body"><h5 class="card-title">' + currentWeatherInfo.location + ' (' + currentWeatherInfo.date + ') ' +
-            '<img id="weather-icon" src="http://openweathermap.org/img/wn/' + currentWeatherInfo.weatherIcon + '@2x.png"></h5>' +
-            '<p class="card-text">Temperature: ' + currentWeatherInfo.temp + ' °F</p>' +
-            '<p class="card-text">Humidity: ' + currentWeatherInfo.humidity + '%</p>' +
-            '<p class="card-text">Wind Speed: ' + currentWeatherInfo.wind + ' MPH</p>' +
-            '<p class="card-text">UV Index: ' + currentWeatherInfo.uvIndex + '</p>')
-        $(".currentCast").append(currentForecast);
 
     });
 }
 
+// function to get and generate a 5-day forecast
 function generateForecast() {
 
     $.ajax({
@@ -83,16 +94,15 @@ function generateForecast() {
     }).then(function (response) {
         console.log(response)
 
-        $("#fiveForecast").empty();
-        $(".forecast-head").empty();
-        $(".forecast-head")
-
+        // This is generte the 5-Day header into .forecast-head
         var forecastHead = $('<h5>5-Day Forecast:</h5>');
         $(".forecast-head").append(forecastHead);
 
+        // This for loop gets the 5-day forecast data after the API call
         for (i = 0; i <= 4; i++) {
             console.log(i)
 
+            // It was easier to make the data into varibles then append it to #fiveForecast
             var newDay = moment().add(1 + i, 'days').format('MM/DD/YYYY');
             var iconForecast = response.list[i].weather[0].icon;
             var iconImgURL = "http://openweathermap.org/img/w/" + iconForecast + ".png";
